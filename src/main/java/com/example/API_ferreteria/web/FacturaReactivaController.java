a3ef4d2e96bdaeaedadcf23d9038a361c34bdf23
mariiana-lp <mariana.lopez.docs_221@gmail.com>
2022-03-30 11:44:11 -0500

    Controller Factura terminada

---
+++ src/main/java/com/example/API_ferreteria/web/FacturaReactivaController.java
@@ -0,0 +1,41 @@
+package com.example.API_ferreteria.web;
+
+import com.example.API_ferreteria.domain.FacturaDTOReactiva;
+import com.example.API_ferreteria.services.IFacturaReactivaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/factura")
+public class FacturaReactivaController {
+
+    @Autowired
+    private IFacturaReactivaService iFacturaReactivaService;
+
+    @PostMapping("/")
+    @ResponseStatus(HttpStatus.CREATED)
+    private Mono<FacturaDTOReactiva> save (FacturaDTOReactiva facturaDTOReactiva){
+        return this.iFacturaReactivaService.save(facturaDTOReactiva);
+    }
+
+    @PutMapping("/{id}")
+    private Mono<ResponseEntity<FacturaDTOReactiva>> delete (@PathVariable("id") String id){
+        return this.iFacturaReactivaService.delete(id)
+                .flatMap(facturaDTOReactiva -> Mono.just(ResponseEntity.ok(facturaDTOReactiva)))
+                .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
+    }
+
+    @GetMapping("/")
+    private Flux<FacturaDTOReactiva> findAll(){
+        return this.iFacturaReactivaService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    private Mono<FacturaDTOReactiva> findById(@PathVariable("id") String id){
+        return this.iFacturaReactivaService.findById(id);
+    }
+}
